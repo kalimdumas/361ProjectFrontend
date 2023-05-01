dda11fe4d272e667aa16955af6e7a415faf89b6e
wasaow <wasaow@outlook.com>
2023-05-01 08:42:37 -0500

    Women Jackets and Shorts

--- project_frontend/src/views/women-jackets.js
+++ project_frontend/src/views/women-jackets.js
@@ -1,22 +1,70 @@
-import React from 'react'
-
 import { Helmet } from 'react-helmet'
+import { Box, Card, CardMedia, CardActions, CardContent, Typography, Rating, IconButton, Grid } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useState, useEffect } from 'react';
 
 import Header from '../components/header'
 import './women-jackets.css'
 
 const WomenJackets = (props) => {
+  const [products, setProducts] = useState([]);
+  const productHandler = (products) => setProducts(products);
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("http://localhost:5257/women-jackets", requestOptions)
+      .then(response => response.json())
+      .then(response => productHandler(response))
+      .catch(error => console.log('error', error))
+  }, [])
+  function handleClick(event) {
+    console.log(event.currentTarget);
+  }
   return (
     <div className="women-jackets-container">
       <Helmet>
         <title>Women-Jackets - 361 Project</title>
         <meta property="og:title" content="Women-Jackets - 361 Project" />
       </Helmet>
-      <Header rootClassName="header-root-class-name5"></Header>
-      <h1 className="women-jackets-text">
-        <span>Women&apos;s Jackets &amp; Hoodies</span>
+      <Header rootClassName="header-root-class-name18"></Header>
+      <h1 className="Women-jackets-text">
+        <span>Women Jackets &amp; Hoodies</span>
         <br></br>
       </h1>
+      <Grid container spacing={2}>
+        {products.map((product, index) => (
+          <Grid item>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 500 }}
+                image={product.image}
+                title={product.name}
+              />
+              <CardContent sx={{ height: 150, overflowY: 'scroll' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box sx={{ display: "flex", alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                  <Rating name="half-rating-read" defaultValue={product.numStars} precision={0.1} readOnly />
+                  <Typography variant="h6" color="text.secondary" sx={{ fontWeight: '700' }}>
+                    ${product.price}
+                  </Typography>
+                  <IconButton onClick={handleClick} color="primary" aria-label="add to shopping cart">
+                    <AddShoppingCartIcon />
+                  </IconButton>
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
--- project_frontend/src/views/women-shorts.js
+++ project_frontend/src/views/women-shorts.js
@@ -1,19 +1,67 @@
-import React from 'react'
-
 import { Helmet } from 'react-helmet'
+import { Box, Card, CardMedia, CardActions, CardContent, Typography, Rating, IconButton, Grid } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useState, useEffect } from 'react';
 
 import Header from '../components/header'
 import './women-shorts.css'
 
 const WomenShorts = (props) => {
+  const [products, setProducts] = useState([]);
+  const productHandler = (products) => setProducts(products);
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("http://localhost:5257/women-shorts", requestOptions)
+      .then(response => response.json())
+      .then(response => productHandler(response))
+      .catch(error => console.log('error', error))
+  }, [])
+  function handleClick(event) {
+    console.log(event.currentTarget);
+  }
   return (
     <div className="women-shorts-container">
       <Helmet>
         <title>Women-Shorts - 361 Project</title>
         <meta property="og:title" content="Women-Shorts - 361 Project" />
       </Helmet>
-      <Header rootClassName="header-root-class-name2"></Header>
-      <h1 className="women-shorts-text">Women&apos;s Shorts</h1>
+      <Header rootClassName="header-root-class-name17"></Header>
+      <h1 className="women-shorts-text">Women Shorts</h1>
+      <Grid container spacing={2}>
+        {products.map((product, index) => (
+          <Grid item>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 500 }}  
+                image={product.image}
+                title={product.name}
+              />
+              <CardContent sx={{ height: 150, overflowY: 'scroll' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box sx={{ display: "flex", alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                  <Rating name="half-rating-read" defaultValue={product.numStars} precision={0.1} readOnly />
+                  <Typography variant="h6" color="text.secondary" sx={{ fontWeight: '700' }}>
+                    ${product.price}
+                  </Typography>
+                  <IconButton onClick={handleClick} color="primary" aria-label="add to shopping cart">
+                    <AddShoppingCartIcon />
+                  </IconButton>
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
