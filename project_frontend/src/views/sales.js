ea3424f0d3c1dbb9717408c7a86fd27d400a6e8c
Kalim Dumas <kdvdumas@gmail.com>
2023-05-04 11:06:29 -0500

    worked on sales pag

--- project_frontend/src/views/sales.js
+++ project_frontend/src/views/sales.js
@@ -1,21 +1,30 @@
-import React from 'react'
-
 import { Helmet } from 'react-helmet'
+import DisplayProducts from '../components/DisplayProducts';
+import { useState, useEffect } from 'react';
 
-import Header from '../components/header'
 import './sales.css'
 
 const Sales = (props) => {
+  const [products, setProducts] = useState([]);
+  const productHandler = (products) => setProducts(products);
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("http://localhost:5257/sales", requestOptions)
+      .then(response => response.json())
+      .then(response => productHandler(response))
+      .catch(error => console.log('error', error))
+  }, [])
   return (
     <div className="sales-container">
       <Helmet>
-        <title>Sales - 361 Project</title>
+        <title>sales - 361 Project</title>
         <meta property="og:title" content="Sales - 361 Project" />
       </Helmet>
-      <h1 className="sales-text">
-        <span>On Sale</span>
-        <br></br>
-      </h1>
+      <h1 className="sales-text">Sales</h1>
+      <DisplayProducts addToCart={props.addToCart} products={products} />
     </div>
   )
 }
